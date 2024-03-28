f27e859727974a4ab87496c4b32a3fd434c9ec6d
Henrique R Almeida <almeida89henrique@gmail.com>
2024-03-28 17:13:12 -0300

    criacao da tabela

---
+++ web-dinamica/js/main.js
@@ -0,0 +1,7 @@
+
+// document.createElement  ( cria um elemento HTML )
+// innerHTML ( define ou obtém a sintaxe HTML descrevendo os elementos descendentes )
+
+
+// document.getElementById  ( recupera algum elemento )
+
